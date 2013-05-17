39157cbf3dd85023572425d2b3a3351e3832c30f
Jesse.Newcomer <jesse.newcomer@gmail.com>
2013-05-17 00:40:28 -0400

    more changes

--- Resources/ui/common/FirstView.js
+++ Resources/ui/common/FirstView.js
@@ -2,7 +2,6 @@
 var _ = require("/lib/underscore");
 
 function FirstView() {
-	//create object instance, a parasitic subclass of Observable
 	var self = Ti.UI.createScrollView({backgroundColor: '#666'});
 	var outputlbl= Ti.UI.createLabel({top:0,left:0,});
 	self.add(outputlbl);
@@ -13,39 +12,29 @@ function FirstView() {
 	var sprite = new Pony();
 	
 	self.add(sprite.createSprite({
-		//spriteStartRow:9,
-		//spriteStartColumn:0,
 		spriteStartFrame:270
 	}));
-  	sprite.spriteSheet.top=0;
+  	sprite.spriteView.top=0;
   	sprite.start({
   		//start:270,
   		//end:278,
-  		animationArray:[270,271,272,273,274,275],
+  		animationArray:[270,271,272,273,274,275,276,277,278],
   		time:3000
+  	});
+  	var sprite2 = new Pony();
+	
+	self.add(sprite2.createSprite({
+		spriteStartFrame:270
+	}));
+  	sprite2.spriteView.top=100;
+  	sprite2.start({
+  		start:270,
+  		end:278,
+  		time:1500
   	});
   	
   	
-  	
-  	/*
-  	for(var i=0; i <40;i++)
-  	{
-	  	var sprite2 = new Pony();
-		
-		self.add(sprite2.createSprite({
-			spriteStartRow:9,
-			spriteStartColumn:0,
-		}));
-	  	sprite2.spriteSheet.top=100*i;
-	  	sprite2.start({
-	  		start:{row:9,column:0},
-	  		end:{row:9,column:9},
-	  		time:1000+(i*100),
-	  	});
-	  	
-  }
 
-  	*/
       
 	return self;
 }
--- Resources/ui/common/sprite.js
+++ Resources/ui/common/sprite.js
@@ -6,17 +6,6 @@ function Sprite() {
 	this.spritesheetImage='/Images/rarity_sprite_sheet_by_urimas-d3fltl0.gif';
 	this.spritesheetWidth=2880;
 	this.spritesheetHeight=1152;
-	this.spritePosition=0;
-	//this.spriteCurrentRow=0;
-	//this.spriteCurrentColumn=0;
-	this.startAnimationFrame;
-	this.endAnimationFrame;
-	this.animationArray;
-	this.spriteCurrentFrame=0;
-	this.top=0;
-	this.left=0;
-	this.spriteTimer;
-	this.animateFrames=0;		
 	this.rows=(this.spritesheetHeight/this.spriteHeight).toFixed();
 	this.columns=(this.spritesheetWidth/this.spriteWidth).toFixed();
 	this.totalFrames = this.rows * this.columns;
@@ -26,90 +15,83 @@ function Sprite() {
 		width:this.spritesheetWidth,
 		height:this.spritesheetHeight,
 	});
-	this.spriteSheet = Ti.UI.createView({
+	this.spriteView = Ti.UI.createView({
 		width:this.spriteWidth,
 		height:this.spriteHeight,
 	});
-	this.spriteSheet.add(this.sheet);
+	this.spriteView.add(this.sheet);
+	
+	this.spritePosition=0;
+	this.spriteCurrentFrame=0;
+	this.startAnimationFrame;
+	this.endAnimationFrame;
+	this.animationArray;
+	
+	
+	this.spriteTimer;
+	this.animateFrames=0;		
+	this.animationDuration=0;
+	
 	return this;
 };
 
 
 
 Sprite.prototype.createSprite = function(constructor) {
-	//if(constructor.spriteStartRow)this.spriteCurrentRow=constructor.spriteStartRow;
-	//if(constructor.spriteStartColumn)this.spriteCurrentColumn=constructor.spriteStartColumn;
 	if(constructor.spriteStart)
-	{
 		this.spriteCurrentFrame=constructor.spriteStartFrame;
-	}else{
+	else
 		this.spriteCurrentFrame=0;
-	}
+		
 	this.setFrame();
-
-	return this.spriteSheet;
+	return this.spriteView;
 };
 
 Sprite.prototype.start = function(args) {
 	var self = this;
-	if(args.start && args.end)
+	if(args.start)
 	{
+		this.startAnimationFrame=args.start;
+		this.endAnimationFrame=args.end;
 		this.animateFrames = args.end-args.start;
 		this.setFrame(args.start);
 	}else
 	{
 		this.animateFrames = args.animationArray.length;
 		this.animationArray=args.animationArray;
+		this.setFrame(this.animationArray[0]);
 	}
-	//Ti.API.info(JSON.stringify(args));
-		
-    this.spriteTimer = setInterval(function(){self.check_time(self,args)}, (args.time/this.animateFrames).toFixed());
+	this.animationDuration=args.time;
+    this.spriteTimer = setInterval(function(){self.check_time(self)}, (this.animationDuration/this.animateFrames).toFixed());
 	return this;
 };
-Sprite.prototype.check_time =function(thisRef,args)
+Sprite.prototype.check_time =function(thisRef)
 {
 	self = thisRef;
-	//var newPosition = -1*(self.spritePosition)*self.spriteWidth;
-	//Ti.API.info('newPosition - ' + newPosition);
-	//self.sheet.left = newPosition;
 
-	
-	
 	if(self.spritePosition++ == self.animateFrames)
 	{
 		self.spritePosition=0;
-		if(args.start && args.end)
-		{
-			this.setFrame(args.start);
-		}else{
-			this.setFrame(args.animationArray[0]);
-		}
-	}else
-	{
-		if(args.start && args.end)
-		{
-			this.setFrame(args.start+self.spritePosition)
-		}else{
-			this.setFrame(args.animationArray[0+self.spritePosition]);
-		}
-		
+		if(self.startAnimationFrame)
+			self.setFrame(self.startAnimationFrame);
+		else
+			self.setFrame(self.animationArray[0]);
 	}
-	
-	return this;
+	else{
+		if(self.startAnimationFrame)
+			self.setFrame(self.startAnimationFrame+self.spritePosition)
+		else
+			self.setFrame(self.animationArray[0+self.spritePosition]);
+	}
+	return self;
 };
 
 
 Sprite.prototype.setFrame =function(frame)
 {
 	if(frame)this.spriteCurrentFrame=frame;
-	var newRow = (this.spriteCurrentFrame/this.columns).toFixed(0);
-	var newColumn = (this.spriteCurrentFrame%this.columns).toFixed(0);
-	var newLeft = -1*newColumn*this.spriteWidth;
-	var newTop = -1*newRow*this.spriteHeight;
-	//Ti.API.info('newPosition - ' + newColumn+','+newRow);
-	this.sheet.left = newLeft;
-	this.sheet.top = newTop;
-	
+	this.sheet.left = -1*(this.spriteCurrentFrame%this.columns).toFixed()*this.spriteWidth;
+	this.sheet.top = -1*(this.spriteCurrentFrame/this.columns).toFixed()*this.spriteHeight;
 	return this;
 };
 
@@ -118,6 +100,12 @@ Sprite.prototype.stop = function() {
 	clearInterval(this.spriteTimer);
 	return this;
 };
+
+Sprite.prototype.resume = function() {
+	this.spriteTimer = setInterval(function(){self.check_time(self)}, (this.animationDuration/this.animateFrames).toFixed());
+	return this;
+};
+
 module.exports = Sprite;
 
 
